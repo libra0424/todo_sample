5c4fc86db4351ca3874694db716161fd14059c6b
ryotana <tanaka0536@youcube.jp>
2020-10-18 23:14:23 +0900

    rails init

---
+++ db/migrate/20201018140945_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name, null:false, comment: 'タスク名'
+      t.boolean :is_done, default: false, null:false, comment: 'タスクの完了'
+
+      t.timestamps
+    end
+  end
+end
