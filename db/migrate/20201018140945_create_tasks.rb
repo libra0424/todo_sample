190c64c542e6fd5b51d56b6e52ebbc0228c7e6ed
ryotana <tanaka0536@youcube.jp>
2020-10-19 00:45:13 +0900

    cssの準備まで

--- db/migrate/20201018140945_create_tasks.rb
+++ db/migrate/20201018140945_create_tasks.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :name, null:false, comment: 'タスク名'
-      t.boolean :is_done, default: false, null:false, comment: 'タスクの完了'
+      t.string :name, null: false, comment: 'タスク名'
+      t.boolean :is_done, default: false, null: false, comment: 'タスクの完了'
 
       t.timestamps
     end
